0a9eddb84ca774fe4815269fe5cf61eb80aafbbb
domin1101 <dominik.winkelbauer@outlook.de>
2016-04-28 09:47:48 +0200

    [BUGFIX] Fixes the skipping of days

--- sigfood/MainPage.xaml.cs
+++ sigfood/MainPage.xaml.cs
@@ -19,7 +19,9 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
-
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.UI.Core;
 // Die Vorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 dokumentiert.
 
 namespace sigfood
@@ -57,10 +59,15 @@ namespace sigfood
                 gotoDetailView();
         }
 
-        private void DayPivot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private async void DayPivot_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             Utility.viewModel.selectedDay = (Day)DayPivot.SelectedItem;
-            (DataContext as MainViewModel).loadNext();   
+
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+            () =>
+            {
+                Utility.viewModel.loadNext();
+            });
         }
 
         private void AdaptiveStates_CurrentStateChanged(object sender, VisualStateChangedEventArgs e)
